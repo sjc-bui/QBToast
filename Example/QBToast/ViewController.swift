528389246cd802825c84d95cc0830528aa760f8f
sjc-bui <bui@setjapan.co.jp>
2021-06-01 16:35:57 +0900

    delete Main.storyboard

--- Example/QBToast/ViewController.swift
+++ Example/QBToast/ViewController.swift
@@ -10,15 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = .systemBlue
+  }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
 }
-
