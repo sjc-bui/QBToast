8d82664686acd3963d99e76641a9d595dad81a00
sjc-bui <bui@setjapan.co.jp>
2021-06-02 14:57:48 +0900

    Add Toast view control

--- Example/QBToast/ViewController.swift
+++ Example/QBToast/ViewController.swift
@@ -10,17 +10,49 @@ import UIKit
 import QBToast
 
 class ViewController: UIViewController {
+  var btn: UIButton?
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-    self.view.backgroundColor = .systemBlue
-    let style = QBToastStyle(backgroundColor: .green)
-    print(style)
+    self.view.backgroundColor = .white
+    btn = UIButton()
+    btn?.setTitle("Click", for: .normal)
+    btn?.titleLabel?.textColor = .white
+    btn?.backgroundColor = .systemBlue
+    btn?.layer.cornerRadius = 6.0
+    btn?.translatesAutoresizingMaskIntoConstraints = false
+    self.view.addSubview(btn!)
+    btn?.widthAnchor.constraint(equalToConstant: 120).isActive = true
+    btn?.heightAnchor.constraint(equalToConstant: 46).isActive = true
+    btn?.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+    btn?.topAnchor.constraint(equalTo: self.view.topAnchor, constant: self.view.bounds.height - (self.view.bounds.height / 4)).isActive = true
+    btn?.addTarget(self, action: #selector(btnClick), for: .touchUpInside)
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  var i = 0
+  @objc func btnClick() {
+    i += 1
+    if i > 3 {
+      i = 1
+    }
+    makeToast(i)
+    print(i)
+  }
+
+  func makeToast(_ rand: Int) {
+    let style = QBToastStyle(cornerRadius: 12.0)
+    switch rand {
+      case 1:
+        self.view.showToast(message: "Dispose of any resources that can be recreated.", style: style, duration: 3.0)
+        break
+      case 2:
+        self.view.showToast(message: "Objective-C", style: style, position: .top, duration: 3.0)
+        break
+      case 3:
+        self.view.showToast(message: "Apple Silicon", style: style, position: .center, duration: 3.0)
+        break
+      default:
+        break
+    }
   }
 }
--- QBToast/Classes/QBToast.swift
+++ QBToast/Classes/QBToast.swift
@@ -9,41 +9,137 @@ import UIKit
 
 extension UIView {
 
-  public func showToast() {
-    
+  public func showToast(message: String?,
+                        style: QBToastStyle = QBToastManager.shared.style,
+                        position: QBToastPosition = QBToastManager.shared.position,
+                        duration: TimeInterval = QBToastManager.shared.duration) {
+    do {
+      let toast = try createToastView(message: message, style: style)
+      self.show(toast: toast, duration: duration, position: position)
+    } catch QBToastError.missingParameters {
+      print("Missing parameters")
+    } catch { }
   }
 
-  private func createToastView() -> UIView {
+  private func createToastView(message: String?, style: QBToastStyle) throws -> UIView {
+
+    guard message != nil else {
+      throw QBToastError.missingParameters
+    }
+
+    var messageLabel: UILabel?
+    if let message = message {
+      messageLabel                  = UILabel()
+      messageLabel?.text            = message
+      messageLabel?.textColor       = style.messageColor
+      messageLabel?.font            = style.messageFont
+      messageLabel?.textAlignment   = NSTextAlignment.center
+      messageLabel?.numberOfLines   = style.messageNumberOfLines
+      messageLabel?.lineBreakMode   = .byTruncatingTail
+      messageLabel?.backgroundColor = .clear
+
+      let maxSize = CGSize(width : self.bounds.size.width  * style.maxWidthPercentage,
+                           height: self.bounds.size.height * style.maxHeightPercentage)
+      let messageSize = messageLabel?.sizeThatFits(maxSize)
+      if let messageSize = messageSize {
+        let actualWidth  = min(messageSize.width , maxSize.width)
+        let actualHeight = min(messageSize.height, maxSize.height)
+        messageLabel?.frame = CGRect(x: 0.0, y: 0.0, width: actualWidth, height: actualHeight)
+      }
+    }
+
+    var messageRect: CGRect = .zero
+    if let messageLabel = messageLabel {
+      messageRect.origin.x    = style.horizontalPadding
+      messageRect.origin.y    = style.verticalPadding
+      messageRect.size.width  = messageLabel.bounds.size.width
+      messageRect.size.height = messageLabel.bounds.size.height
+    }
+
     let wrapView = UIView()
+    wrapView.autoresizingMask = [.flexibleTopMargin,
+                                 .flexibleRightMargin,
+                                 .flexibleBottomMargin,
+                                 .flexibleLeftMargin]
+    wrapView.frame = CGRect(x: 0.0,
+                            y: 0.0,
+                            width: messageRect.size.width + (style.horizontalPadding * 2),
+                            height: messageRect.size.height + (style.verticalPadding * 2))
+    wrapView.backgroundColor = style.backgroundColor
+    wrapView.layer.roundCorner(radius: style.cornerRadius)
+
+    if let messageLabel = messageLabel {
+      messageLabel.frame = messageRect
+      wrapView.addSubview(messageLabel)
+    }
     return wrapView
   }
 
-  private func show() {
-    
+  private func show(toast: UIView, duration: TimeInterval, position: QBToastPosition) {
+    let startpoint = position.startPoint(forToastView: toast, inSuperView: self)
+    let point = position.centerPoint(forToastView: toast, inSuperView: self)
+
+    toast.center = startpoint
+    toast.alpha = 0
+
+    self.addSubview(toast)
+    UIView.animate(withDuration: QBToastManager.shared.style.fadeDuration,
+                   delay: 0.0,
+                   usingSpringWithDamping: 0.8,
+                   initialSpringVelocity: 0.65,
+                   options: .curveEaseIn) {
+      toast.alpha = 1
+      toast.center = point
+    } completion: { _ in
+      let timer = Timer(timeInterval: duration,
+                        target: self,
+                        selector: #selector(self.toastTimer(_:)),
+                        userInfo: toast,
+                        repeats: false)
+      RunLoop.main.add(timer, forMode: .common)
+    }
   }
 
-  private func hide() {
-    
+  @objc func toastTimer(_ timer: Timer) {
+    guard let toast = timer.userInfo as? UIView else { return }
+    hide(toast)
+  }
+
+  private func hide(_ toast: UIView) {
+    var currentPoint = toast.center
+    let centerYPoint = self.bounds.size.height / 2
+
+    if currentPoint.y > centerYPoint {
+      currentPoint.y += (toast.frame.size.height * 2)
+    } else {
+      currentPoint.y -= (toast.frame.size.height * 2)
+    }
+
+    UIView.animate(withDuration: QBToastManager.shared.style.fadeDuration,
+                   delay: 0.0,
+                   usingSpringWithDamping: 0.8,
+                   initialSpringVelocity: 0.65,
+                   options: .curveEaseOut) {
+      toast.alpha = 0
+      toast.center = currentPoint
+    } completion: { _ in
+      toast.removeFromSuperview()
+    }
   }
 }
 
+private enum QBToastError: Error {
+  case missingParameters
+}
+
 public struct QBToastStyle {
-
+  
+  /** Toast view background color. Default `.black(0.8)`*/
   let backgroundColor: UIColor
-
-  let titleColor: UIColor
-
-  let titleFont: UIFont
-
-  let titleAlignment: NSTextAlignment
-
-  let titleNumberOfLines: Int
 
   let messageColor: UIColor
 
   let messageFont: UIFont
-
-  let messageAlignment: NSTextAlignment
 
   let messageNumberOfLines: Int
 
@@ -51,38 +147,36 @@ public struct QBToastStyle {
 
   let maxHeightPercentage: CGFloat
 
+  let horizontalPadding: CGFloat
+
+  let verticalPadding: CGFloat
+
   let cornerRadius: CGFloat
-  
-  let fadeDuration: CGFloat
+
+  let fadeDuration: TimeInterval
 
   public init(
-    backgroundColor: UIColor = .black.withAlphaComponent(0.8),
-    titleColor: UIColor = .white,
-    titleFont: UIFont = .boldSystemFont(ofSize: 16.0),
-    titleAlignment: NSTextAlignment = .left,
-    titleNumberOfLines: Int = 0,
+    backgroundColor: UIColor = .black,
     messageColor: UIColor = .white,
     messageFont: UIFont = .systemFont(ofSize: 16.0),
-    messageAlignment: NSTextAlignment = .left,
     messageNumberOfLines: Int = 0,
     maxWidthPercentage: CGFloat = 0.8,
     maxHeightPercentage: CGFloat = 0.8,
+    horizontalPadding: CGFloat = 12.0,
+    verticalPadding: CGFloat = 12.0,
     cornerRadius: CGFloat = 4.0,
-    fadeDuration: CGFloat = 0.2
+    fadeDuration: TimeInterval = 0.4
   ) {
-    self.backgroundColor = backgroundColor
-    self.titleColor = titleColor
-    self.titleFont = titleFont
-    self.titleAlignment = titleAlignment
-    self.titleNumberOfLines = titleNumberOfLines
-    self.messageColor = messageColor
-    self.messageFont = messageFont
-    self.messageAlignment = messageAlignment
+    self.backgroundColor      = backgroundColor.withAlphaComponent(0.7)
+    self.messageColor         = messageColor
+    self.messageFont          = messageFont
     self.messageNumberOfLines = messageNumberOfLines
-    self.maxWidthPercentage = maxWidthPercentage
-    self.maxHeightPercentage = maxHeightPercentage
-    self.cornerRadius = cornerRadius
-    self.fadeDuration = fadeDuration
+    self.maxWidthPercentage   = maxWidthPercentage
+    self.maxHeightPercentage  = maxHeightPercentage
+    self.horizontalPadding    = horizontalPadding
+    self.verticalPadding      = verticalPadding
+    self.cornerRadius         = cornerRadius
+    self.fadeDuration         = fadeDuration
   }
 }
 
@@ -92,7 +186,7 @@ public class QBToastManager {
 
   public var style = QBToastStyle()
 
-  public var duration: TimeInterval = 3.0
+  public var duration: TimeInterval = 0.3
 
   public var position: QBToastPosition = .bottom
 
@@ -106,18 +200,50 @@ public enum QBToastPosition {
   case center
   case bottom
 
-  fileprivate func centerPoint() -> CGPoint {
-    return CGPoint(x: 0, y: 0)
+  fileprivate func centerPoint(forToastView    toast: UIView,
+                               inSuperView superview: UIView) -> CGPoint {
+
+    let topPadding = QBToastManager.shared.style.verticalPadding + superview.safeArea.top
+    let bottomPadding = QBToastManager.shared.style.verticalPadding + superview.safeArea.bottom
+
+    switch self {
+      case .top:
+        return CGPoint(x: superview.bounds.size.width / 2,
+                       y: (toast.frame.size.height / 2) + topPadding)
+      case .center:
+        return CGPoint(x: superview.bounds.size.width / 2,
+                       y: superview.bounds.size.height / 2)
+      case .bottom:
+        return CGPoint(x: superview.bounds.size.width / 2,
+                       y: superview.bounds.size.height - (toast.frame.size.height / 2) - bottomPadding)
+    }
+  }
+
+  fileprivate func startPoint(forToastView toast: UIView,
+                              inSuperView superview: UIView) -> CGPoint {
+    switch self {
+      case .top:
+        return CGPoint(x: superview.bounds.size.width / 2,
+                       y: -toast.frame.size.height)
+      case .center:
+        return CGPoint(x: superview.bounds.size.width / 2,
+                       y: superview.bounds.size.height / 2 - toast.frame.size.height)
+      case .bottom:
+        return CGPoint(x: superview.bounds.size.width / 2,
+                       y: superview.bounds.size.height + toast.frame.size.height)
+    }
   }
 }
 
-fileprivate extension UIView {
-  var safeArea: UIEdgeInsets {
-    if #available(iOS 11.0, *) {
-      return self.safeAreaInsets
-    } else {
-      return .zero
+extension UIApplication {
+    var keyWindowInConnectedScenes: UIWindow? {
+        return windows.first(where: { $0.isKeyWindow })
     }
+}
+
+private extension UIView {
+  var safeArea: UIEdgeInsets {
+    return UIApplication.shared.delegate?.window??.safeAreaInsets ?? .zero
   }
 }
 
